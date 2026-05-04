e5dd73d0d15de38ce3323ae92631895b6686b3e2
Maud Bernard-Verdier <maudbv@gmail.com>
2026-05-04 17:00:44 +0200

    update cross-scoring

--- Script/cross-scoring.R
+++ Script/cross-scoring.R
@@ -21,28 +21,74 @@ cross_UWS <- uws_paris_iris %>%
              by = c("ID_site" = "ID_site") )
 
 
-## Regression lineaire katia vs Maria ----
 
-ggplot(data = cross_UWS,
-       mapping = aes(x = `Total wildness`, y = `Total wildness_maria`)) +
-  geom_point() +
-  geom_smooth(method = "lm", col = "red") +
-  geom_abline(slope = 1, intercept =0 ) +
-  ylim (0,5) + xlim (0,5)
+## Comparaison  katia vs Maria ----
 
 ## test de correlation de spearman (correlation des rangs) ----
 cor.test(cross_UWS$`Total wildness`, 
          cross_UWS$`Total wildness_maria`,
           method = 'spearman')
 
-## test de correlation de spearman (correlation des rangs) ----
-summary( cross_reg <- lm(`Total wildness_maria`~`Total wildness`,data = cross_UWS))
-plot(simulateResiduals(cross_reg)) # not bad
+## Linear regression (RMS) ----
+summary( cross_reg <- lm(`Total wildness_maria`~`Total wildness`, data = cross_UWS))
+plot(simulateResiduals(cross_reg)) # not perfect
+
+summary( cross_reg_0 <- lm(`Total wildness_maria`~0 +`Total wildness`, data = cross_UWS))
+plot(simulateResiduals(cross_reg_0)) # not perfect
 
 # R2 = 0.65 ; Equation : UWS_maria =  -0,2703 + 1,0633 * UWS_katia
 # Regression not horrible but not very tight.
-
 # Need to check against rescored katia data
 
+## SMA (Single major axis estimation) ----
+library(smatr)
+
+#### Free intercept :  ----
+(cross_sma <- sma(`Total wildness_maria`~ `Total wildness`, data = cross_UWS))
+plot(cross_sma)
+plot(cross_sma , which = "residual")
+
+### Regression SMA forced through 0 intercept:----
+(cross_sma_0 <- sma(`Total wildness_maria`~ 0 + `Total wildness`,data = cross_UWS))
+plot(cross_sma_0)
+
+# Graph ----
 
 
+## Simple linear regression graph ----
+ggplot(data = cross_UWS,
+       mapping = aes(x = `Total wildness`, y = `Total wildness_maria`)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, color = "grey",
+                linetype = "solid", linetype = "dotted") +
+  geom_abline(aes(slope =  cross_reg$coefficients[2],
+                  intercept =  cross_reg$coefficients[1]),  col = "darkblue") +
+  ylim (0,5) + xlim (0,5) +
+  geom_text(mapping = aes(x = x, y = y, label = label),
+            data = data_frame(x = c(2.5, 2.5), 
+                              y = c(1, 0.7), 
+                              label = c("R2 = 0.65",
+                                        "UWS_maria =  -0,2703 + 1,0633 * UWS_katia")),
+            hjust = "left", size =3) +
+  theme_minimal()
+
+## Model comparison graph ----
+ggplot(data = cross_UWS,
+       mapping = aes(x = `Total wildness`, y = `Total wildness_maria`)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, color = "grey",linetype = "solid") +
+  geom_abline(aes(slope =  cross_reg$coefficients[2],
+                      intercept =  cross_reg$coefficients[1],
+                      col = "lm",linetype = "free")) +
+  geom_abline(aes(slope = cross_reg_0$coefficients, intercept = 0,
+              col = "lm",linetype = "via0")) +
+  geom_abline(aes(slope = cross_sma$groupsummary$Slope,
+              intercept = cross_sma$groupsummary$Int,
+              col = "sma",linetype = "free")) +
+  geom_abline(aes(slope = cross_sma_0$groupsummary$Slope,
+              intercept = 0,
+              col = "sma", linetype = "via0")) +
+  scale_linetype_manual(name = "Intercept", values = c("free" = "solid", "via0" = "dashed")) + 
+  scale_color_manual(name = "Models", values = c("lm" = "darkblue", "sma" = "red")) + 
+  ylim (0,5) + xlim (0,5) +
+  theme_minimal()
